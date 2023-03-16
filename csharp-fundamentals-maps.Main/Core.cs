2b8cc8dc6333d4acd3a0d3b6cb3287620d50a8e7
uerbzr <nigel.sibbert@gmail.com>
2023-03-16 16:32:41 +0000

    Completed exercises and tests.

--- csharp-fundamentals-maps.Main/Core.cs
+++ csharp-fundamentals-maps.Main/Core.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace csharp_fundamentals_maps_exemplar.Main
 {
-    public class Exercise
+    public class Core
     {
         /*
     The final fundamental building block of C# is a Map. There is still much to learn about the language,
@@ -48,26 +48,39 @@ namespace csharp_fundamentals_maps_exemplar.Main
             in the createPerson method
          */
 
+        public string getValue(string key)
+        {
+            var dict = this.createPerson();
 
+            return dict[key];
+            
+        }
 
+          //TODO:  2. Create a method named hasKey that accepts two parameters:
         /*
-            2. Create a method named hasKey that accepts two parameters:
-            - A HashMap of String, String key value pairs
-            - A string
+            - A Dictionary of string, string key value pairs            
+            - string
             The method must return a boolean that represents whether the string provided exists as a key
             in the provided HashMap
          */
-
+         public bool hasKey(Dictionary<string,string> dictionary, string isitthere)
+         {
+            return dictionary.ContainsKey(isitthere);
+         }
 
 
         /*
             3. Create a method named getValueOrDefault that accepts two parameters:
-            - A HashMap of String, Integer key value pairs
+            - A Dictionary<string,integer>
             - A string
             The method must use the string provided to return the integer contained in the provided HashMap,
             or -1 if the string provided is not a key in the HashMap
          */
-
+        public int getValueOrDefault(Dictionary<string,int> dictionary, string isitthere)
+        {            
+            return dictionary.TryGetValue(isitthere, out int actual) ? actual : -1;
+            
+        }
 
 
         /*
@@ -78,27 +91,43 @@ namespace csharp_fundamentals_maps_exemplar.Main
             [23, 19, 96, 23, 165]   |   ArrayList<String> ["chicken", "nice", "chicken", "soup"]
             [918, 71, 88]           |   ArrayList<String> []
          */
-        public ArrayList<String> buildSecretPhrase(ArrayList<Integer> numbers)
+        public string[] buildSecretPhrase(int[] numbers)
         {
             // Do not modify the map
             Dictionary<int, string> map = new Dictionary<int, string>();
-            map.put(23, "chicken");
-            map.put(42, "universe");
-            map.put(165, "soup");
-            map.put(6712, "bass");
-            map.put(7, "muse");
-            map.put(96, "nice");
+            map.Add(23, "chicken");
+            map.Add(42, "universe");
+            map.Add(165, "soup");
+            map.Add(6712, "bass");
+            map.Add(7, "muse");
+            map.Add(96, "nice");
             // Write your code below this comment...
+                        
+            int elements = 0;
+            foreach(int i in numbers)
+            {
+                if (map.ContainsKey(i))
+                {
+                    elements++;
+                }
+            }
+            
+            string[] result = new string[elements];
+            int y = 0;
+            foreach(int x in numbers)
+            {
+                if(map.ContainsKey(x))
+                {
+                    result[y]=map[x];
+                    y++;
+                }
 
+                
+            }
+            return result;
+            
 
-
-
-            // ...and above this comment
-        }
-
-        public double getValue(string v)
-        {
-            throw new NotImplementedException();
-        }
+            //    // ...and above this comment
+        }            
     }
 }
---
+++ csharp-fundamentals-maps.Test/CoreTest.cs
@@ -0,0 +1,67 @@
+﻿
+using csharp_fundamentals_maps_exemplar.Main;
+using NUnit.Framework;
+
+namespace csharp_fundamentals_maps.Test
+{
+    public class CoreTest
+    {
+        Core exercise;
+
+        public CoreTest()
+        {
+            this.exercise = new Core();
+        }
+
+        [Test]
+        [TestCase("Nathan", "firstName")]
+        [TestCase("King", "lastName")]
+        [TestCase("Software Developer", "occupation")]
+        public void shouldReturnMappedValues(string expected, string key)
+        {
+            Assert.AreEqual(expected, this.exercise.createPerson()[key]);
+        }
+
+        [Test]
+        [TestCase(new string[] { "universe", "bass", "muse" }, new int[] { 42, 6712, 7 })]
+        [TestCase(new string[] { "chicken", "nice", "chicken", "soup" }, new int[] { 23, 19, 96, 23, 165 })]
+        [TestCase(new string[] { }, new int[] { 918, 71, 88})]
+        public void shouldhavecreatedalistofstrings(string[] phrase, int[] array)
+        {
+            Assert.AreEqual(phrase,this.exercise.buildSecretPhrase(array));
+        }
+        [Test]
+        [TestCase("Earth",true)]
+        [TestCase("Venus", false)]
+        [TestCase("Jupiter", true)]
+        [TestCase("Mercury", true)]
+        [TestCase("Uranus", false)]
+        [TestCase("Saturn", false)]
+        [TestCase("Neptune", false)]
+        public void doeskeyexistinarray(string planet,bool expectedresult)
+        {
+            Dictionary<string,string> planets = new Dictionary<string,string>();
+            planets.Add("Earth", "Its a nice planet which is great to live on");
+            planets.Add("Jupiter", "A large planet which isn't too great to live on");
+            planets.Add("Mercury", "A warm planet which isn't too great to live on");
+
+
+            Assert.AreEqual(expectedresult, this.exercise.hasKey(planets, planet));
+
+        }
+
+        [Test]
+        [TestCase(true,"RobinReliant",3)]
+        [TestCase(true, "Bicycle", 2)]
+        [TestCase(true, "Train", -1)]
+        public void test(bool expectedresult,string vehicle,int wheelcount)
+        {
+            Dictionary<string,int> dictionary = new Dictionary<string,int>();
+            dictionary.Add("Quad", 4);
+            dictionary.Add("Bicycle", 2);
+            dictionary.Add("RobinReliant", 3);
+
+            Assert.AreEqual(expectedresult, this.exercise.getValueOrDefault(dictionary, vehicle)==wheelcount);
+        }
+    }
+}
