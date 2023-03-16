0339171bdf5bb217f2add033d3c56ac6a1b6ee44
john <johnny.lioup@gmail.com>
2023-03-16 11:12:38 +0000

    updated exemplar repo

--- csharp-fundamentals-maps.Main/Exercise.cs
+++ csharp-fundamentals-maps.Main/Exercise.cs
@@ -4,31 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace csharp_fundamentals_maps.Main
+namespace csharp_fundamentals_maps_exemplar.Main
 {
     public class Exercise
     {
         /*
-            The final fundamental building block of C# is a Map. There is still much to learn about the language,
-            but this component will allow you to start building lots of more complex pieces of software.
-            A map is like a list, except we have control over what we use for an item's index. This allows us to use
-            types other than an incrementing integer to describe the positions of the data contained in the structure.
-            Like Lists, Maps have a variety of different implementations. We'll focus on the HashMap in this exercise.
-            We create a HashMap like this:
-            HashMap<K, V>
-            K is where you'd put the data type of the key for an item, V is the data type of the value.
-            If we wanted to map a persons details (their first name, last name, occupation etc.), we could use
-            a HashMap using a String key and a String value like so:
-            HashMap<String, String>
-         */
+    The final fundamental building block of C# is a Map. There is still much to learn about the language,
+    but this component will allow you to start building lots of more complex pieces of software.
+    A map is like a list, except we have control over what we use for an item's index. This allows us to use
+    types other than an incrementing integer to describe the positions of the data contained in the structure.
+    Like Lists, Maps have a variety of different implementations. We'll focus on the HashMap in this exercise.
+    We create a HashMap like this:
+    HashMap<K, V>
+    K is where you'd put the data type of the key for an item, V is the data type of the value.
+    If we wanted to map a persons details (their first name, last name, occupation etc.), we could use
+    a HashMap using a String key and a String value like so:
+    HashMap<String, String>
+ */
 
         /*
             Spend some time understanding the method below
-            - It creates a HashMap of String, String key value pairs
+            - It creates a Map of String, String key value pairs
             - It adds some keys with values
-            - Under the hood, the Map would look like this: { "firstName": "Nathan", "lastName": King, etc. }
-            - It returns the HashMap
-            https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/util/Map.html
+            - Under the hood, the Dictionary would look like this: { "firstName": "Nathan", "lastName": King, etc. }
+            - It returns the Dictionary
+            
          */
         public Dictionary<string, string> createPerson()
         {
@@ -81,7 +81,7 @@ namespace csharp_fundamentals_maps.Main
         public ArrayList<String> buildSecretPhrase(ArrayList<Integer> numbers)
         {
             // Do not modify the map
-            HashMap<Integer, String> map = new HashMap<>();
+            Dictionary<int, string> map = new Dictionary<int, string>();
             map.put(23, "chicken");
             map.put(42, "universe");
             map.put(165, "soup");
