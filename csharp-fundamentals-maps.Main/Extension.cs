c9ae05366f0760bfaaa4c09d9f31eac97c8ae893
uerbzr <nigel.sibbert@gmail.com>
2023-05-03 14:34:25 +0100

    updated Core,Extension plus tests

---
+++ csharp-fundamentals-maps.Main/Extension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals_maps.Main
+{
+    public class Extension
+    {
+        private Dictionary<string, float> _planets;
+
+        public Extension()
+        {
+            _planets = new Dictionary<string, float>();
+            _planets.Add("Jupiter", 5.2f);
+            _planets.Add("Uranus", 19.2f);
+            //_planets.Add("Pluto", 39f);
+            _planets.Add("Mercury", 0.39f);
+            _planets.Add("Saturn", 9.54f);
+            _planets.Add("Earth", 1f);
+            _planets.Add("Mars", 1.52f);
+            _planets.Add("Venus", 0.72f);
+            _planets.Add("Neptune", 30.06f);
+        }
+        //TODO   Pluto is unfortunately no longer a planet so please comment out the add line!
+
+
+        public Dictionary<string, int> LettersInName()
+        {
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            //TODO   Complete this method to return an Dictionary of <string,int> which contains 
+            //          the planet name and the number of letters in its name
+            //          iterate the _planets using a foreach object to load the result dictionary.
+
+            foreach (KeyValuePair<string, float> kvp in _planets)
+            {
+                result.Add(kvp.Key, kvp.Key.Length);
+            }
+
+
+            return result;
+        }
+
+
+
+
+
+
+        public Dictionary<string, float> OrderedPlanets()
+        {
+            return _planets.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        }
+        public Dictionary<string, float> OrderedPlanetsByDescending()
+        {
+            return _planets.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        }
+        //TODO:  modify the OrderedPlanetsByDescending so it is not dictionary is not doing an OrderBy but OrderByDescending
+
+
+
+        //      
+        //TODO   using the OrderedPlanets method get the
+        //       closest to the sun and furthest from the sun.  Update the methods to return the correct KeyValuePair's Key (the string)!
+        //       Note you need either the First or Last item in the Dictionary depending on 
+
+
+        public string FurthestFromTheSun()
+        {
+            KeyValuePair<string, float> result = OrderedPlanets().Last();
+
+            return result.Key;
+        }
+        public string ClosestToTheSun()
+        {
+            KeyValuePair<string, float> result = OrderedPlanets().First();
+
+            return result.Key;
+        }
+
+        public Dictionary<string, float> Planets { get { return _planets; } }
+
+
+    }
+}
---
+++ csharp-fundamentals-maps.Test/ExtensionTests.cs
@@ -0,0 +1,65 @@
+﻿using csharp_fundamentals_maps.Main;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_fundamentals_maps.Test
+{
+    [TestFixture]
+    public class ExtensionTests
+    {
+        private Extension ext;
+        public ExtensionTests()
+        {
+            ext = new Extension();
+
+        }
+
+        [Test]
+        public void PlutoNotAPlanet()
+        {
+
+            Assert.IsFalse(ext.Planets.ContainsKey("Pluto"));
+        }
+
+        [Test]
+        public void LettersInNameTest()
+        {
+            foreach (KeyValuePair<string, int> item in ext.LettersInName())
+            {
+                Assert.IsTrue(item.Key.Length == item.Value);
+            }
+        }
+
+        [Test]
+        [TestCase("Mercury", 7)]
+        [TestCase("Venus", 6)]
+        [TestCase("Earth", 5)]
+        [TestCase("Mars", 4)]
+        [TestCase("Jupiter", 3)]
+        [TestCase("Saturn", 2)]
+        [TestCase("Uranus", 1)]
+        [TestCase("Neptune", 0)]
+        public void PlanetOrderTest(string planet, int orderfromsun)
+        {
+
+            Assert.IsTrue(ext.OrderedPlanetsByDescending().ToArray()[orderfromsun].Key == planet);
+        }
+
+
+        [Test]
+        public void FurthestFromTheSunTest()
+        {
+            Assert.AreEqual(ext.FurthestFromTheSun().ToLower(), "neptune");
+        }
+        [Test]
+        public void ClosestToTheSunTest()
+        {
+            Assert.AreEqual(ext.ClosestToTheSun().ToLower(), "mercury");
+        }
+
+    }
+}
